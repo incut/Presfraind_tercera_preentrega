9fa9b41e8be146a52d13ad6072c1d70996b10ce3
incut <augustopresfraind@gmail.com>
2024-06-10 19:29:03 -0300

    creacion de pagina inicio

---
+++ inicio/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from inicio import views
+urlpatterns = [
+   path('', views.inico, name= 'inicio'),
+]
---
+++ inicio/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def inico (request):
+    return render(request, 'inicio/inicio.html')
+# Create your views here.
